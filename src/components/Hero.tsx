306cb975324c36ab37f2f0c5e389de0fcd02d658
HamzaSx <Hamzasxh@gmail.com>
2024-12-12 11:06:46 +0300

    feat(Hero page videos):
    - Adding:
        A variable of total videos.
        A function getVideoUrl.
        An array of length of totalVideos, then it will loop over it to
    generate a div that contain video.
        For every video src it will use getVideoUrl function to get the
    source of the video.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,6 +5,7 @@ import { useInterval } from "usehooks-ts";
 
 function Hero() {
   // NOTE: The total number of videos
+  const totalVideos = 4;
 
   // NOTE: States: ---------------------------------------------------
 
@@ -36,6 +37,9 @@ function Hero() {
     },
     mouseActive ? 300 : null,
   );
+
+  // Get the video url
+  const getVideoUrl = (index: number) => `videos/hero-${index + 1}.mp4`;
 
   // NOTE: Animations: ---------------------------------------------------
 
@@ -286,73 +290,24 @@ function Hero() {
         }}
       >
         {/* NOTE: An array of video frames */}
-        <div
-          className={`h-full w-full absolute top-0 left-0 rouded-lg`}
-          id={`video-frame`}
-        >
-          {/* NOTE: Div container that has svg for video mask and border */}
-          <div
-            id="mask-container"
-            style={{
-              width: "var(--container-full-size)",
-              height: "var(--container-full-size)",
-              borderRadius: "var(--rx)",
-            }}
-            className="cursor-pointer absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50"
-            onClick={() => setMiniVidChangeAnimation(!miniVidChangeAnimation)}
-            onMouseEnter={() => {
-              setMaskHover(true);
-            }}
-            onMouseLeave={() => {
-              setMaskHover(false);
-            }}
-          >
+        {Array.from({ length: totalVideos }, (_, i) => (
+          <>
+            {/* NOTE: Div container that has svg for video mask and border */}
             <div
-              id="mask-border"
-              style={{
-                width: "calc(var(--full-size) - 2px + var(--full-screan-w))",
-                height: "calc(var(--full-size) - 2px + var(--full-screan-h))",
-                borderRadius: "calc(var(--rx) - 2px)",
-                transform: `translate(calc(var(--translate-w) - var(--half-size) + var(--mouse-x) - 50vw),calc(var(--translate-h) - var(--half-size) + var(--mouse-y) - 50vh)) rotateX(var(--rotate-x)) rotateY(var(--rotate-y))`,
-                transformBox: "border-box",
-                outlineWidth: "0px",
-              }}
-              className="outline outline-black absolute top-1/2 left-1/2 z-[60]"
-            ></div>
-            <svg
-              viewBox="0 0 300 300"
-              xmlns="http://www.w3.org/2000/svg"
-              className="absolute w-full h-full"
+              className={`h-full w-full absolute top-0 left-0 rouded-lg`}
+              id={`video-frame-${i}`}
+              key={i}
             >
-              <mask id="mask" maskUnits="objectBoundingBox">
-                <rect
-                  id="mask-rect"
-                  rx="var(--rx)"
-                  fill="white"
-                  y={0}
-                  x={0}
-                  style={{
-                    width: "calc(var(--full-size) + var(--full-screan-w))",
-                    height: "calc(var(--full-size) + var(--full-screan-h))",
-                    transform: `translate(calc(var(--translate-w) - var(--half-size) + var(--mouse-x)), calc(var(--translate-h) - var(--half-size) + var(--mouse-y))) rotateX(var(--rotate-x)) rotateY(var(--rotate-y))`,
-                    transformOrigin: "50% 50%",
-                    transformBox: "border-box",
-                  }}
-                />
-              </mask>
-            </svg>
-          </div>
-          <video
-            style={{
-              mask: "url(#mask)",
-            }}
-            id="current-video"
-            src="/videos/hero-1.mp4"
-            className={`absolute top-0 left-0 w-full h-full object-cover object-center origin-center`}
-            muted
-            loop
-          />
-        </div>
+              <video
+                id={`current-video-${i}`}
+                src={getVideoUrl(i)}
+                className={`absolute top-0 left-0 w-full h-full object-cover object-center origin-center`}
+                muted
+                loop
+              />
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );
