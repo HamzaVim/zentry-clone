efbcd7dfbbc626d243321e2acad4b3f3369be28f
HamzaSx <Hamzasxh@gmail.com>
2024-12-12 11:12:44 +0300

    refactor(Deactivate animation):
    - For now all animation is deactivated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -54,228 +54,228 @@ function Hero() {
   });
 
   // When the `mask-container` is hovered ---------------------------------------------------
-  useGSAP(
-    () => {
-      if (
-        !timelineHoverRef.current ||
-        !timelineMouseActiveRef.current ||
-        !animationLoaded ||
-        !timelineMiniVidChangeRef.current
-      )
-        return;
-      timelineMouseActiveRef.current.clear();
-      timelineHoverRef.current.clear();
-      timelineMiniVidChangeRef.current.clear();
-
-      if (maskHover) {
-        timelineHoverRef.current
-          .to("#mask-rect, #mask-border", {
-            duration: 0.5,
-            ease: "none",
-            "--full-size": "18rem",
-            "--half-size": "9rem",
-            "--rx": "0.4rem",
-          })
-          .to("#mask-rect, #mask-border", {
-            duration: 0.7,
-            ease: "power1.inOut",
-            "--full-size": "19.5rem",
-            "--half-size": "9.75rem",
-            yoyo: true,
-            repeat: -1,
-          });
-      } else {
-        timelineHoverRef.current.to("#mask-rect, #mask-border", {
-          overwrite: true,
-          duration: 0.7,
-          ease: "none",
-          "--full-size": "18rem",
-          "--half-size": "9rem",
-          yoyo: false,
-          repeat: 0,
-        });
-      }
-    },
-    { dependencies: [maskHover] },
-  );
+  // useGSAP(
+  //   () => {
+  //     if (
+  //       !timelineHoverRef.current ||
+  //       !timelineMouseActiveRef.current ||
+  //       !animationLoaded ||
+  //       !timelineMiniVidChangeRef.current
+  //     )
+  //       return;
+  //     timelineMouseActiveRef.current.clear();
+  //     timelineHoverRef.current.clear();
+  //     timelineMiniVidChangeRef.current.clear();
+  //
+  //     if (maskHover) {
+  //       timelineHoverRef.current
+  //         .to("#mask-rect, #mask-border", {
+  //           duration: 0.5,
+  //           ease: "none",
+  //           "--full-size": "18rem",
+  //           "--half-size": "9rem",
+  //           "--rx": "0.4rem",
+  //         })
+  //         .to("#mask-rect, #mask-border", {
+  //           duration: 0.7,
+  //           ease: "power1.inOut",
+  //           "--full-size": "19.5rem",
+  //           "--half-size": "9.75rem",
+  //           yoyo: true,
+  //           repeat: -1,
+  //         });
+  //     } else {
+  //       timelineHoverRef.current.to("#mask-rect, #mask-border", {
+  //         overwrite: true,
+  //         duration: 0.7,
+  //         ease: "none",
+  //         "--full-size": "18rem",
+  //         "--half-size": "9rem",
+  //         yoyo: false,
+  //         repeat: 0,
+  //       });
+  //     }
+  //   },
+  //   { dependencies: [maskHover] },
+  // );
 
   // Parallax Effect for `mask-rect` & `mask-border` ---------------------------------------------------
   const heroRef = useRef<HTMLDivElement>(null);
-  const { contextSafe } = useGSAP();
-
-  const handleMouseMove = useCallback(
-    contextSafe((e: MouseEvent) => {
-      // Get the mouse position
-      const x = e.clientX;
-      const y = e.clientY;
-
-      // Get the middle of the screen / object
-      const middleX = window.innerWidth / 2;
-      const middleY = window.innerHeight / 2;
-
-      // Get offset from middle
-      const offsetX = x - middleX;
-      const offsetY = y - middleY;
-
-      // For mouse movement
-      const mouseX = offsetX * 0.2;
-      const mouseY = offsetY * 0.2;
-
-      // For parallax effect
-      const parallaxY = (offsetX / middleX) * 45;
-      const parallaxX = (offsetY / middleY) * 45;
-
-      gsap.to("#mask-rect, #mask-border", {
-        overwrite: "auto",
-        duration: 0,
-        ease: "none",
-        "--mouse-x": mouseX,
-        "--mouse-y": mouseY,
-        "--rotate-x": `${-1 * parallaxX}deg`,
-        "--rotate-y": `${parallaxY}deg`,
-      });
-    }),
-    [],
-  );
-  useGSAP(
-    () => {
-      if (!heroRef.current) return;
-
-      if (!miniVidChangeAnimation) {
-        heroRef.current.addEventListener("mousemove", handleMouseMove);
-      } else {
-        heroRef.current.removeEventListener("mousemove", handleMouseMove);
-      }
-
-      return () => {
-        heroRef.current?.removeEventListener("mousemove", handleMouseMove);
-      };
-    },
-    { dependencies: [miniVidChangeAnimation] },
-  );
+  // const { contextSafe } = useGSAP();
+  //
+  // const handleMouseMove = useCallback(
+  //   contextSafe((e: MouseEvent) => {
+  //     // Get the mouse position
+  //     const x = e.clientX;
+  //     const y = e.clientY;
+  //
+  //     // Get the middle of the screen / object
+  //     const middleX = window.innerWidth / 2;
+  //     const middleY = window.innerHeight / 2;
+  //
+  //     // Get offset from middle
+  //     const offsetX = x - middleX;
+  //     const offsetY = y - middleY;
+  //
+  //     // For mouse movement
+  //     const mouseX = offsetX * 0.2;
+  //     const mouseY = offsetY * 0.2;
+  //
+  //     // For parallax effect
+  //     const parallaxY = (offsetX / middleX) * 45;
+  //     const parallaxX = (offsetY / middleY) * 45;
+  //
+  //     gsap.to("#mask-rect, #mask-border", {
+  //       overwrite: "auto",
+  //       duration: 0,
+  //       ease: "none",
+  //       "--mouse-x": mouseX,
+  //       "--mouse-y": mouseY,
+  //       "--rotate-x": `${-1 * parallaxX}deg`,
+  //       "--rotate-y": `${parallaxY}deg`,
+  //     });
+  //   }),
+  //   [],
+  // );
+  // useGSAP(
+  //   () => {
+  //     if (!heroRef.current) return;
+  //
+  //     if (!miniVidChangeAnimation) {
+  //       heroRef.current.addEventListener("mousemove", handleMouseMove);
+  //     } else {
+  //       heroRef.current.removeEventListener("mousemove", handleMouseMove);
+  //     }
+  //
+  //     return () => {
+  //       heroRef.current?.removeEventListener("mousemove", handleMouseMove);
+  //     };
+  //   },
+  //   { dependencies: [miniVidChangeAnimation] },
+  // );
 
   // When the mouse is active/not active ---------------------------------------------------
-  useGSAP(
-    () => {
-      if (
-        !timelineMouseActiveRef.current ||
-        !timelineHoverRef.current ||
-        maskHover ||
-        !animationLoaded ||
-        miniVidChangeAnimation
-      )
-        return;
-
-      timelineHoverRef.current.clear();
-
-      if (mouseActive) {
-        const active = timelineMouseActiveRef.current.isActive();
-        timelineMouseActiveRef.current.clear();
-        if (active) {
-          timelineMouseActiveRef.current.to("#mask-rect, #mask-border", {
-            duration: 1,
-            ease: "none",
-            "--full-size": "18rem",
-            "--half-size": "9rem",
-            "--rx": "0.4rem",
-            outlineWidth: 2,
-          });
-        } else {
-          timelineMouseActiveRef.current
-            .to("#mask-border", {
-              overwrite: true,
-              duration: 0,
-              ease: "none",
-              outlineWidth: 2,
-            })
-            .to("#mask-rect, #mask-border", {
-              duration: 0,
-              ease: "none",
-              "--rx": "0.1rem",
-            })
-            .to("#mask-rect, #mask-border", {
-              duration: 1,
-              ease: "none",
-              "--full-size": "18rem",
-              "--half-size": "9rem",
-              "--rx": "0.4rem",
-            });
-        }
-      } else {
-        timelineMouseActiveRef.current.clear();
-        timelineMouseActiveRef.current
-          .to("#mask-rect, #mask-border", {
-            overwrite: true,
-            duration: 1,
-            ease: "power1.out",
-            "--full-size": "6rem",
-            "--half-size": "3rem",
-            "--rx": "0.4rem",
-            outlineWidth: 2,
-          })
-          .to(
-            "#mask-rect, #mask-border",
-            {
-              duration: 1,
-              ease: "none",
-              "--full-size": "0rem",
-              "--half-size": "0rem",
-              "--rx": "0.1rem",
-            },
-            "-=0.2",
-          )
-          .to("#mask-rect, #mask-border", {
-            duration: 0,
-            ease: "none",
-            outlineWidth: 0,
-            "--rx": "0rem",
-          });
-      }
-    },
-    { dependencies: [mouseActive] },
-  );
+  // useGSAP(
+  //   () => {
+  //     if (
+  //       !timelineMouseActiveRef.current ||
+  //       !timelineHoverRef.current ||
+  //       maskHover ||
+  //       !animationLoaded ||
+  //       miniVidChangeAnimation
+  //     )
+  //       return;
+  //
+  //     timelineHoverRef.current.clear();
+  //
+  //     if (mouseActive) {
+  //       const active = timelineMouseActiveRef.current.isActive();
+  //       timelineMouseActiveRef.current.clear();
+  //       if (active) {
+  //         timelineMouseActiveRef.current.to("#mask-rect, #mask-border", {
+  //           duration: 1,
+  //           ease: "none",
+  //           "--full-size": "18rem",
+  //           "--half-size": "9rem",
+  //           "--rx": "0.4rem",
+  //           outlineWidth: 2,
+  //         });
+  //       } else {
+  //         timelineMouseActiveRef.current
+  //           .to("#mask-border", {
+  //             overwrite: true,
+  //             duration: 0,
+  //             ease: "none",
+  //             outlineWidth: 2,
+  //           })
+  //           .to("#mask-rect, #mask-border", {
+  //             duration: 0,
+  //             ease: "none",
+  //             "--rx": "0.1rem",
+  //           })
+  //           .to("#mask-rect, #mask-border", {
+  //             duration: 1,
+  //             ease: "none",
+  //             "--full-size": "18rem",
+  //             "--half-size": "9rem",
+  //             "--rx": "0.4rem",
+  //           });
+  //       }
+  //     } else {
+  //       timelineMouseActiveRef.current.clear();
+  //       timelineMouseActiveRef.current
+  //         .to("#mask-rect, #mask-border", {
+  //           overwrite: true,
+  //           duration: 1,
+  //           ease: "power1.out",
+  //           "--full-size": "6rem",
+  //           "--half-size": "3rem",
+  //           "--rx": "0.4rem",
+  //           outlineWidth: 2,
+  //         })
+  //         .to(
+  //           "#mask-rect, #mask-border",
+  //           {
+  //             duration: 1,
+  //             ease: "none",
+  //             "--full-size": "0rem",
+  //             "--half-size": "0rem",
+  //             "--rx": "0.1rem",
+  //           },
+  //           "-=0.2",
+  //         )
+  //         .to("#mask-rect, #mask-border", {
+  //           duration: 0,
+  //           ease: "none",
+  //           outlineWidth: 0,
+  //           "--rx": "0rem",
+  //         });
+  //     }
+  //   },
+  //   { dependencies: [mouseActive] },
+  // );
 
   // Mini video when change it will increase the size of the mask to fill the screen ---------------------------------------------------
-  useGSAP(
-    () => {
-      if (
-        !timelineHoverRef.current ||
-        !timelineMouseActiveRef.current ||
-        !animationLoaded ||
-        !timelineMiniVidChangeRef.current
-      )
-        return;
-      timelineMouseActiveRef.current.clear();
-      timelineHoverRef.current.clear();
-      timelineMiniVidChangeRef.current.clear();
-
-      if (miniVidChangeAnimation) {
-        timelineMiniVidChangeRef.current
-          .to("#mask-rect, #mask-border", {
-            overwrite: true,
-            duration: 0.2,
-            ease: "power1.out",
-            "--mouse-x": 0,
-            "--mouse-y": 0,
-            "--rotate-x": "0deg",
-            "--rotate-y": "0deg",
-            yoyo: false,
-            repeat: 0,
-          })
-          .to("#mask-rect, #mask-border", {
-            duration: 1,
-            ease: "power1.out",
-            "--full-screan-h": "101dvh",
-            "--full-screan-w": "102vw",
-            "--half-size": "0rem",
-            "--full-size": "0rem",
-            "--translate-w": "-1vw",
-            "--translate-h": "-1vh",
-          });
-      }
-    },
-    { dependencies: [miniVidChangeAnimation] },
-  );
+  // useGSAP(
+  //   () => {
+  //     if (
+  //       !timelineHoverRef.current ||
+  //       !timelineMouseActiveRef.current ||
+  //       !animationLoaded ||
+  //       !timelineMiniVidChangeRef.current
+  //     )
+  //       return;
+  //     timelineMouseActiveRef.current.clear();
+  //     timelineHoverRef.current.clear();
+  //     timelineMiniVidChangeRef.current.clear();
+  //
+  //     if (miniVidChangeAnimation) {
+  //       timelineMiniVidChangeRef.current
+  //         .to("#mask-rect, #mask-border", {
+  //           overwrite: true,
+  //           duration: 0.2,
+  //           ease: "power1.out",
+  //           "--mouse-x": 0,
+  //           "--mouse-y": 0,
+  //           "--rotate-x": "0deg",
+  //           "--rotate-y": "0deg",
+  //           yoyo: false,
+  //           repeat: 0,
+  //         })
+  //         .to("#mask-rect, #mask-border", {
+  //           duration: 1,
+  //           ease: "power1.out",
+  //           "--full-screan-h": "101dvh",
+  //           "--full-screan-w": "102vw",
+  //           "--half-size": "0rem",
+  //           "--full-size": "0rem",
+  //           "--translate-w": "-1vw",
+  //           "--translate-h": "-1vh",
+  //         });
+  //     }
+  //   },
+  //   { dependencies: [miniVidChangeAnimation] },
+  // );
 
   return (
     <div className="relative min-h-screen w-screen overflow-x-hidden">
