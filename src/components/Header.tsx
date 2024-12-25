b14c8caeb7e1596d98787967095a7e1b9f6b183c
HamzaSx <Hamzasxh@gmail.com>
2024-12-25 17:48:05 +0300

    feat(Header: Animation):
    - Now the header has animation when scroll.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import { TiLocationArrow } from "react-icons/ti";
 import Button from "./Button";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import DownArrow from "./svgs/DownArrow";
 
 function Header({ showHeader }: { showHeader: "show" | "float" | "hide" }) {
@@ -26,6 +28,9 @@ function Header({ showHeader }: { showHeader: "show" | "float" | "hide" }) {
   // Ref for the item that is used for the animation
   const navItemSlectRef = useRef<HTMLLIElement>(null);
 
+  // State when the navigation item is selected
+  const [selected, setSelected] = useState(false);
+
   // NOTE: Functions: ---------------------------------------------------
 
   // Get the width of the navigation items
@@ -34,13 +39,137 @@ function Header({ showHeader }: { showHeader: "show" | "float" | "hide" }) {
       setNavItemsWidth(navItemsRef.current.map((item) => item.offsetWidth));
     }
   }, []);
+  const getX = (index: number) => {
+    const fromTo = navItemsWidth.slice(0, index);
+
+    const total = fromTo.reduce((acc, curr) => acc + curr, 0);
+    return total;
+  };
 
   // NOTE: Animation: ---------------------------------------------------
+
+  useGSAP(() => {
+    if (!navItemSlectRef.current || !navItemsRef.current) return;
+
+    if (navHover) {
+      gsap.to(navItemSlectRef.current, {
+        overwrite: "auto",
+        duration: 0,
+        visibility: "visible",
+        width: navItemsWidth[navItemHover!],
+        height: "100%",
+        x: getX(navItemHover!),
+        onComplete: () => {
+          setSelected(true);
+        },
+      });
+      gsap.to(navItemsRef.current[navItemHover!], {
+        overwrite: "auto",
+        duration: 0,
+        color: "black",
+      });
+    } else {
+      setSelected(false);
+      if (navItemHover === null) {
+        gsap.to(navItemSlectRef.current, {
+          overwrite: "auto",
+          duration: 0,
+          visibility: "hidden",
+          width: 0,
+          height: 0,
+          x: 0,
+        });
+      } else {
+        gsap.to(navItemSlectRef.current, {
+          overwrite: "auto",
+          duration: 0,
+          visibility: "hidden",
+          width: 0,
+          height: 0,
+          x: 0,
+        });
+
+        navItemsRef.current.forEach((item) => {
+          gsap.to(item, {
+            overwrite: true,
+            duration: 0,
+            ease: "power4.in",
+            color: "#DFDFF2",
+          });
+        });
+      }
+    }
+  }, [navHover]);
+
+  useGSAP(() => {
+    if (!navItemSlectRef.current || !navItemsRef.current || !selected) return;
+
+    gsap.to(navItemSlectRef.current, {
+      x: getX(navItemHover!),
+      width: navItemsWidth[navItemHover!],
+      duration: 0.4,
+    });
+    gsap.to(navItemsRef.current[navItemHover!], {
+      duration: 0,
+      delay: 0.2,
+      ease: "power4.in",
+      color: "black",
+    });
+    navItemsRef.current.forEach((item, index) => {
+      if (index !== navItemHover) {
+        gsap.to(item, {
+          duration: 0.2,
+          ease: "power4.in",
+          color: "#DFDFF2",
+        });
+      }
+    });
+  }, [navItemHover]);
+
+  useGSAP(
+    () => {
+      if (showHeader === "show") {
+        gsap.to("#header", {
+          overwrite: "auto",
+          duration: 0.4,
+          ease: "power4.in",
+          background: "rgba(0,0,0,0)",
+          top: "0.75rem",
+          y: "0%",
+          borderColor: "transparent",
+        });
+      } else if (showHeader === "hide") {
+        gsap.to("#header", {
+          overwrite: "auto",
+          duration: 0.3,
+          ease: "none",
+          top: "0rem",
+          y: "-100%",
+        });
+
+        gsap.set("#header", {
+          overwrite: "auto",
+          delay: 0.3,
+          background: "rgba(0,0,0,1)",
+          borderColor: "rgb(255,255,255,0.15)",
+        });
+      } else {
+        gsap.to("#header", {
+          overwrite: "auto",
+          duration: 0.3,
+          ease: "none",
+          top: "0.75rem",
+          y: "0%",
+        });
+      }
+    },
+    { dependencies: [showHeader] },
+  );
 
   return (
     <header
       id="header"
-      className="py-5 rounded-xl border border-none text-textColor fixed left-6 right-6 z-[10000]"
+      className="py-5 rounded-xl border text-textColor fixed left-6 right-6 z-[10000]"
     >
       <div className="flex justify-between items-center ">
         <div className="flex gap-7">
